218fae2d16c53c6fc43f957b93377556516f05af
Luis Osio Chico <72286405+LOsioChico@users.noreply.github.com>
2024-05-19 09:23:33 -0500

    style: Update indentation to 2 spaces, change yarn for pnpm and format with prettier

--- src/lib/problem.ts
+++ src/lib/problem.ts
@@ -3,35 +3,34 @@ import { ProblemDifficulty, ProblemStatus, Uris } from "../utils/interfaces";
 import Submission from "./submission";
 
 class Problem {
+  static uris: Uris;
 
-    static uris: Uris;
+  static setUris(uris: Uris): void {
+    Problem.uris = uris;
+  }
 
-    static setUris(uris: Uris): void {
-        Problem.uris = uris;
-    }
+  constructor(
+    readonly slug: string,
+    public id?: number,
+    public title?: string,
+    public difficulty?: ProblemDifficulty,
+    public starred?: boolean,
+    public locked?: boolean,
+    public likes?: number,
+    public dislikes?: number,
+    public status?: ProblemStatus,
+    public tag?: Array<string>,
+    public totalAccepted?: number,
+    public totalSubmission?: number,
 
-    constructor(
-        readonly slug: string,
-        public id?: number,
-        public title?: string,
-        public difficulty?: ProblemDifficulty,
-        public starred?: boolean,
-        public locked?: boolean,
-        public likes?: number,
-        public dislikes?: number,
-        public status?: ProblemStatus,
-        public tag?: Array<string>,
-        public totalAccepted?: number,
-        public totalSubmission?: number,
+    public sampleTestCase?: string,
+    public content?: string,
+    public codeSnippets?: Array<any>
+  ) {}
 
-        public sampleTestCase?: string,
-        public content?: string,
-        public codeSnippets?: Array<any>,
-    ) { }
-
-    async detail(): Promise<Problem> {
-        const response = await Helper.GraphQLRequest({
-            query: `
+  async detail(): Promise<Problem> {
+    const response = await Helper.GraphQLRequest({
+      query: `
                 query getQuestionDetail($titleSlug: String!) {
                     question(titleSlug: $titleSlug) {
                         questionId
@@ -56,40 +55,40 @@ class Problem {
                     }
                 }
             `,
-            variables: {
-                titleSlug: this.slug,
-            }
-        });
-        const question = response.question;
-        this.id = Number(question.questionId);
-        this.title = question.title;
-        this.difficulty = Helper.difficultyMap(question.difficulty);
-        this.starred = question.isLiked !== null;
-        this.locked = question.isPaidOnly;
-        this.likes = question.likes;
-        this.dislikes = question.dislikes;
-        this.status = Helper.statusMap(question.status);
-        this.tag = question.topicTags.map(function (t: any) {
-            return t.name;
-        });
-        const stats: any = JSON.parse(question.stats);
-        this.totalAccepted = stats.totalAcceptedRaw;
-        this.totalSubmission = stats.totalSubmissionRaw;
+      variables: {
+        titleSlug: this.slug,
+      },
+    });
+    const question = response.question;
+    this.id = Number(question.questionId);
+    this.title = question.title;
+    this.difficulty = Helper.difficultyMap(question.difficulty);
+    this.starred = question.isLiked !== null;
+    this.locked = question.isPaidOnly;
+    this.likes = question.likes;
+    this.dislikes = question.dislikes;
+    this.status = Helper.statusMap(question.status);
+    this.tag = question.topicTags.map(function (t: any) {
+      return t.name;
+    });
+    const stats: any = JSON.parse(question.stats);
+    this.totalAccepted = stats.totalAcceptedRaw;
+    this.totalSubmission = stats.totalSubmissionRaw;
 
-        this.sampleTestCase = question.sampleTestCase;
-        this.content = question.content;
-        this.codeSnippets = question.codeSnippets;
-        return this;
-    }
+    this.sampleTestCase = question.sampleTestCase;
+    this.content = question.content;
+    this.codeSnippets = question.codeSnippets;
+    return this;
+  }
 
-    async getSubmissions(): Promise<Array<Submission>> {
-        const submissions: Array<Submission> = [];
-        let offet = 0;
-        const limit = 20;
-        let hasNext = true;
-        while (hasNext) {
-            const response = await Helper.GraphQLRequest({
-                query: `
+  async getSubmissions(): Promise<Array<Submission>> {
+    const submissions: Array<Submission> = [];
+    let offet = 0;
+    const limit = 20;
+    let hasNext = true;
+    while (hasNext) {
+      const response = await Helper.GraphQLRequest({
+        query: `
                 query Submissions($offset: Int!, $limit: Int!, $questionSlug: String!) {
                     submissionList(offset: $offset, limit: $limit, questionSlug: $questionSlug) {
                         lastKey
@@ -107,43 +106,45 @@ class Problem {
                     }
                 }
                 `,
-                variables: {
-                    offset: offet,
-                    limit: limit,
-                    questionSlug: this.slug,
-                }
-            });
+        variables: {
+          offset: offet,
+          limit: limit,
+          questionSlug: this.slug,
+        },
+      });
 
-            hasNext = response.submissionList.hasNext;
-            const submission: Array<any> = response.submissionList.submissions;
-            offet += submission.length;
-            submission.map(s => {
-                submissions.push(new Submission(
-                    Number(s.id),
-                    s.isPending,
-                    s.lang,
-                    s.memory,
-                    s.runtime,
-                    Helper.submissionStatusMap(s.statusDisplay),
-                    Number(s.timestamp),
-                ));
-            });
-        }
-        return submissions;
+      hasNext = response.submissionList.hasNext;
+      const submission: Array<any> = response.submissionList.submissions;
+      offet += submission.length;
+      submission.map((s) => {
+        submissions.push(
+          new Submission(
+            Number(s.id),
+            s.isPending,
+            s.lang,
+            s.memory,
+            s.runtime,
+            Helper.submissionStatusMap(s.statusDisplay),
+            Number(s.timestamp)
+          )
+        );
+      });
     }
+    return submissions;
+  }
 
-    async submit(lang: string, code: string): Promise<Submission> {
-        const response = await Helper.HttpRequest({
-            url: Problem.uris.submit.replace("$slug", this.slug),
-            method: "POST",
-            body: {
-                lang: lang,
-                "question_id": this.id,
-                "typed_code": code,
-            }
-        });
-        return new Submission(JSON.parse(response).submission_id);
-    }
+  async submit(lang: string, code: string): Promise<Submission> {
+    const response = await Helper.HttpRequest({
+      url: Problem.uris.submit.replace("$slug", this.slug),
+      method: "POST",
+      body: {
+        lang: lang,
+        question_id: this.id,
+        typed_code: code,
+      },
+    });
+    return new Submission(JSON.parse(response).submission_id);
+  }
 }
 
 export default Problem;
--- src/utils/interfaces.ts
+++ src/utils/interfaces.ts
@@ -1,57 +1,65 @@
 interface HttpRequestOptions {
-    method?: string;
-    url: string;
-    referer?: string;
-    resolveWithFullResponse?: boolean;
-    form?: any;
-    body?: any;
+  method?: string;
+  url: string;
+  referer?: string;
+  resolveWithFullResponse?: boolean;
+  form?: any;
+  body?: any;
 }
 
 interface GraphQLRequestOptions {
-    origin?: string;
-    referer?: string;
-    query: string;
-    variables?: object;
+  origin?: string;
+  referer?: string;
+  query: string;
+  variables?: object;
 }
 
 interface Credit {
-    session?: string;
-    csrfToken: string;
+  session?: string;
+  csrfToken: string;
 }
 
 enum ProblemStatus {
-    "Accept",
-    "Not Accept",
-    "Not Start"
+  "Accept",
+  "Not Accept",
+  "Not Start",
 }
 
 enum ProblemDifficulty {
-    "Easy",
-    "Medium",
-    "Hard",
+  "Easy",
+  "Medium",
+  "Hard",
 }
 
 enum SubmissionStatus {
-    "Accepted",
-    "Compile Error",
-    "Wrong Answer",
-    "Time Limit Exceeded",
+  "Accepted",
+  "Compile Error",
+  "Wrong Answer",
+  "Time Limit Exceeded",
 }
 
 enum EndPoint {
-    "US",
-    "CN",
+  "US",
+  "CN",
 }
 
 interface Uris {
-    base: string;
-    login: string;
-    graphql: string;
-    problemsAll: string;
-    problem: string;
-    submit: string;
-    submission: string;
+  base: string;
+  login: string;
+  graphql: string;
+  problemsAll: string;
+  problem: string;
+  submit: string;
+  submission: string;
 }
 
-export { HttpRequestOptions, GraphQLRequestOptions, Credit, ProblemStatus, ProblemDifficulty, SubmissionStatus, EndPoint, Uris, };
-
+export {
+  HttpRequestOptions,
+  GraphQLRequestOptions,
+  Credit,
+  ProblemStatus,
+  ProblemDifficulty,
+  SubmissionStatus,
+  EndPoint,
+  Uris,
+};
